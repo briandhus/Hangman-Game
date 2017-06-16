4b8b4ea391aef3d127a842f19b3c3c93eedd3ecd
Revinate <revinate@Revinates-MacBook-Air.local>
2017-06-16 00:00:49 -0700

    hangman logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,52 @@
 var titles = ["better off dead", "the goonies", "star wars", "gremlins", "aliens", "the usual suspects", "groundhog day", "animal house", "casablanca", "psycho", "the godfather", "dr strangelove", "jurassic park", "poltergeist", "apocalypse now", "goodfellas", "pulp fiction", "edward scissorhands", "beetlejuice", "jungle fever"];
-var word = "";
+var renderedString = "";
 var wins = 0;
 var losses = 0;
 var ties = 0;
 
-confirm("Are you ready to play Hollywood Hangman?");
+
+
+// computer establishes random movie title
 
 var randomTitle = titles[Math.floor(Math.random() * titles.length)];
+console.log(randomTitle);
 
-document.onkeyup = function(event) {
-        
+// computer renders length of title in underscores
+for (var i = 0; i < randomTitle.length; i++) {
+	if (randomTitle[i] === " ") {
+		renderedString += " ";		
+	} else {
+		renderedString += "_";
+	}
+	
+}
+
+console.log(renderedString)
+
+
+// user picks a letter
+
+document.onkeypress = function(event) {
+     
 	var userGuess = event.key;
+
+	for(var i = 0; i < randomTitle.length; i++) {
+		if (userGuess === randomTitle[i]) {
+			renderedString[i] = randomTitle[i];
+			console.log(renderedString);
+		}
+
+	}
+
 };
+
+// letter is correct, fills in blanks, letter is no longer an option
+
+// letter is incorrect, letter no longer option
+//  letter is shown on screen as used
+
+// hangman counter counts down for incorrect answers
+// if reaches 0, game over, 
+// count loss
+// if title completed game over,
+// count win
